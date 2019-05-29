9fb27f3a69baa5ab42b8e48f7b8fa3088026f464
Kamesh312 <kamesh312@gmail.com>
2019-05-29 11:50:42 +0530

    Update Gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,13 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON("package.json"),
-    
+    sass: {
+      dist: {
+        files: {
+          'menu.css': 'menu.scss'
+        }
+      }
+    },
     uglify: {
       "presets": [
         "stage-3"
@@ -12,13 +18,22 @@ module.exports = function(grunt) {
               'menu_parse.min.js': 'menu_parse.js'
           }
       }
-  }
+  },
+  concat: {
+      options: {
+        separator: ';',
+      },
+      dist: {
+        src: ['menu.min.js', 'menu_parse.min.js'],
+        dest: 'menu-compressed.min.js',
+      },
+    }
   });
-  // Load Grunt plugins
-  //grunt.loadNpmTasks("grunt-contrib-sass");
+  // Load Grunt plugin
+  grunt.loadNpmTasks("grunt-contrib-sass");
   grunt.loadNpmTasks("grunt-contrib-uglify");
   //grunt.loadNpmTasks('grunt-contrib-concat');
 
   // Register Grunt tasks
-  grunt.registerTask("default", ["uglify"]);
+  grunt.registerTask("default", ["sass", "uglify"]);
 };
